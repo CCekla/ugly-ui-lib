1530fe6f7a1d536e7407f71754d010fc1b3f457d
Ccekla <28961003+CCekla@users.noreply.github.com>
2022-08-06 17:11:47 +0200

    added dropdowns. Not sure if is getting as ugly as I'd like...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import Tooltip from './ui/tooltip';
-
+import Dropdown from './ui/dropdown';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init();
+
+//create a dropdown
+const drops = document.querySelectorAll('.dropdown');
+drops.forEach(drop => {
+    const dropdown = new Dropdown(drop);
+    dropdown.init();
+});
